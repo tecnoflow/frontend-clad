3f75601880f22eb42da2342125e2216a2207bcc8
cristian <cristianarangodaza@gmail.com>
2024-09-24 17:18:01 -0500

    testing routers

--- src/main.tsx
+++ src/main.tsx
@@ -2,25 +2,37 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
-import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import { createHashRouter, RouterProvider } from 'react-router-dom'
 import { MainForm } from './pages/firstform/MainForm.tsx'
 
-const router = createBrowserRouter([
+
+const router = createHashRouter([
   {
     element: <App />,
     children: [
-     /*  { path: "/", element: <IndexPage /> },
+      { path: "/", element:  <MainForm /> },
+      { path: "/sign-in", element:  <MainForm /> },
+      { path: "/form/:id", element: <MainForm /> },
+    ]
+  }
+])
+
+/* const router = createBrowserRouter([
+  {
+    element: <App />,
+    children: [
+      { path: "/", element: <IndexPage /> },
       { path: "/sign-in", element: <SignIn appearance={{
         elements: {
           footer: {
             display: 'none',
           },
         },
-      }} /> }, */
+      }} /> },
       { path: "/form/:id", element: <MainForm /> },
     ]
   }
-])
+]) */
 
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
